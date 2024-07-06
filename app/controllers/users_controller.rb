ae4086c031709eab72390830234fa9e91507aabe
nkroker <nikhilq.75@gmail.com>
2024-07-07 01:53:13 +0530

    Added controller and model with route and also done some code displacement for better quering

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  # GET /users
+  # Fetches all users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # POST /users
+  # Creates a new user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # GET /users/filter
+  # Filters users by campaign names
+  def filter
+    campaign_names = params[:campaign_names].split(',')
+    @users = User.with_campaigns(campaign_names)
+    render json: @users
+  end
+
+  private
+
+  # Strong parameters for creating a user
+  def user_params
+    params.require(:user).permit(:name, :email, campaigns_list: [])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# app/models/user.rb
 class User < ApplicationRecord
+
+  # Class method to filter users based on campaign names in their campaigns_list
+  def self.with_campaigns(campaign_names)
+    sanitized_campaigns = sanitize_sql_for_conditions(["JSON_CONTAINS(campaigns_list, ?)", campaign_names.map { |name| { campaign_name: name } }.to_json])
+    where(sanitized_campaigns)
+  end
 end
