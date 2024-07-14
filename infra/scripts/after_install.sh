03973ab5e611791379f9f4a1e42654a622c8c638
nkroker <nikhilq.75@gmail.com>
2024-07-14 20:47:28 +0530

    Changed the database changes

--- infra/scripts/after_install.sh
+++ infra/scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/app
-gem install bundler -v 2.5.10
+sudo gem install bundler -v 2.5.10
 bundle _2.5.10_ install
-RAILS_ENV=production bundle exec rails db:migrate
-RAILS_ENV=production bundle exec rails assets:precompile
+bundle exec rails db:prepare
